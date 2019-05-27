499e655a8de8c65a62aee64bc4fa25cf0afb4dbe
Michael Traver <michael.r.traver@gmail.com>
2019-05-26 20:51:04 -0700

    Make web UI nicer: center buttons, make them bigger

--- cmd/server/template.go
+++ cmd/server/template.go
@@ -4,24 +4,33 @@ const indexTemplate = `
 <!doctype html>
 <html lang="en">
 	<head>
-	  <meta charset="utf-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	  <title>RPi IR Remote</title>
+		<title>RPi IR Remote</title>
+
+		<link rel="stylesheet"
+		 href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		 integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+		 crossorigin="anonymous">
 	</head>
 	<body>
-		<p>Did you know that the wavelength of infrared radiation ranges from about 800 nm to 1 mm?</p>
-		<p>Remote: {{ .Remote.Name }}</p>
+		<div class="container mt-1">
+			<div class="col-sm text-center">
+				<p>Did you know that the wavelength of infrared radiation ranges from about 800 nm to 1 mm?</p>
+				<p>Remote: {{ .Remote.Name }}</p>
 
-		{{ range $name, $command := .Remote.Commands }}
-			<button type="button" class="button" name="{{ $name }}" id="button-{{ $name }}">{{ $name }}</button>
-			<br>
-			<br>
-		{{ end }}
+				{{ range $name, $command := .Remote.Commands }}
+					<button type="button" class="btn btn-primary" name="{{ $name }}" id="button-{{ $name }}">{{ $name }}</button>
+					<br>
+					<br>
+				{{ end }}
+			</div>
+		</div>
 	</body>
 
 	<script type="text/javascript">
-		var buttons = document.getElementsByClassName('button');
+		var buttons = document.getElementsByClassName('btn');
 		for (var i = 0; i < buttons.length; i++) {
 			buttons[i].addEventListener('click', event => {
 				fetch('/' + event.srcElement.getAttribute('name'), {method: 'POST'});
